62c4037279ad203dfa4c84662d0e749faef5103a
SinghVivek96 <itsvivek96@gmail.com>
2021-07-13 21:41:08 +0530

    Initial commit

---
+++ src/main/java/com/vivek/employee101/Employee101Application.java
@@ -0,0 +1,13 @@
+package com.vivek.employee101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Employee101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Employee101Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/vivek/employee101/Employee101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivek.employee101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Employee101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
